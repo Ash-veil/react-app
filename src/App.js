77d178e3a5a4e2b0a4271b05c3a53d3831c966e4
ash-veil <thyra.veil@gmail.com>
2025-08-24 13:52:58 +0100

    template integration

--- src/App.js
+++ src/App.js
@@ -1,11 +1,36 @@
 
+import NavBar from "./components/navBar";
+import SideBar from "./components/sideBar";
+import CardsRow from "./components/cardsRow";
+import Charts from "./components/charts";
+//import Table from "./components/table";
+import Footer from "./components/footer";
+
 function App() {
+  
   return (
-    <div >
-        <p>
-          react--app
-        </p>
+  <div>
+  <NavBar />
+
+  <div id="layoutSidenav">
+    <SideBar />
+    <div id="layoutSidenav_content">
+      <main>
+        <div className="container-fluid px-4">
+          <h1 className="mt-4">Dashboard</h1>
+          <ol className="breadcrumb mb-4">
+            <li className="breadcrumb-item active">Dashboard</li>
+          </ol>
+          <CardsRow />
+          <Charts />
+          {/* <Table /> */}
+        </div>
+      </main>
+     <Footer />
     </div>
+  </div>
+</div>
+
   );
 }
 
---
+++ src/components/charts.js
@@ -0,0 +1,28 @@
+import React from 'react'
+
+const Charts = () => {
+  return (
+    <div className="row">
+            <div className="col-xl-6">
+              <div className="card mb-4">
+                <div className="card-header">
+                  <i className="fas fa-chart-area me-1" />
+                  Area Chart Example
+                </div>
+                <div className="card-body"><canvas id="myAreaChart" width="100%" height={40} /></div>
+              </div>
+            </div>
+            <div className="col-xl-6">
+              <div className="card mb-4">
+                <div className="card-header">
+                  <i className="fas fa-chart-bar me-1" />
+                  Bar Chart Example
+                </div>
+                <div className="card-body"><canvas id="myBarChart" width="100%" height={40} /></div>
+              </div>
+            </div>
+          </div>
+  )
+}
+
+export default Charts
