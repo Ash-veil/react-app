427138ec935727a56643cd25cb88ca031401c6c3
ash-veil <thyra.veil@gmail.com>
2025-08-28 18:59:51 +0100

    update customer page

--- src/pages/customers/customers.js
+++ src/pages/customers/customers.js
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import "./cardStyle.css";
 import ReactPaginate from "react-paginate";
 import BreadCrumb from "../../components/partials/breadCrumb";
 import { getCustomers } from "../../api/customers";
@@ -23,7 +24,7 @@ const Customers = () => {
   return (
     <div className="container-fluid px-4">
       <BreadCrumb currentPage="Customers" />
-      <div className="row">
+      {/* <div className="row">
         {currentCustomers.map((item) => (
           <div className="col-xl-3 col-md-6 border-primary" key={item.id}>
             <div className="card bg-secondary text-white mb-4">
@@ -60,6 +61,122 @@ const Customers = () => {
             nextLinkClassName={"page-link"}
             disabledClassName={"disabled"}
           />
+        </div>
+      </div> */}
+      <div class="row align-items-center">
+        <div class="col-md-6">
+          <div class="mb-3">
+            <h5 class="card-title">
+              Contact List <span class="text-muted fw-normal ms-2">({customers.length})</span>
+            </h5>
+          </div>
+        </div>
+        <div class="col-md-6">
+          <div class="d-flex flex-wrap align-items-center justify-content-end gap-2 mb-3">
+            <div>
+              <a
+                href="#"
+                class="btn btn-primary"
+              >
+                <i class="bx bx-plus me-1"></i> Add New
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="row">
+        {currentCustomers.map((item) => (
+          <div class="col-xl-3 col-sm-6">
+            <div class="card">
+              <div class="card-body">
+                <div class="dropdown float-end">
+                  <a
+                    class="text-muted dropdown-toggle font-size-16"
+                    href="#"
+                    role="button"
+                    data-bs-toggle="dropdown"
+                    aria-haspopup="true"
+                  >
+                    <i class="bx bx-dots-horizontal-rounded"></i>
+                  </a>
+                  <div class="dropdown-menu dropdown-menu-end">
+                    <a class="dropdown-item" href="#">
+                      Edit
+                    </a>
+                    <a class="dropdown-item" href="#">
+                      Action
+                    </a>
+                    <a class="dropdown-item" href="#">
+                      Remove
+                    </a>
+                  </div>
+                </div>
+                <div class="d-flex align-items-center">
+                  <div class="avatar-md">
+                    <div class="avatar-title bg-soft-primary text-primary display-6 m-0 rounded-circle">
+                      <i class="fas fa-md fa-user"></i>
+                    </div>
+                  </div>
+                  <div class="flex-1 ms-3">
+                    <h5 class="font-size-16 mb-1">
+                      <a href="#" class="text-dark">
+                        {item.name}
+                      </a>
+                    </h5>
+                    <span class="badge badge-soft-success mb-0">
+                      {item.description}
+                    </span>
+                  </div>
+                </div>
+                <div class="mt-3 pt-1">
+                  <p class="text-muted mb-0">
+                    <i class="fas fa-lg fa-phone font-size-15 align-middle pe-2 text-primary"></i>{" "}
+                    {item.phone}
+                  </p>
+                  <p class="text-muted mb-0 mt-2">
+                    <i class="fas fa-lg fa-envelope font-size-15 align-middle pe-2 text-primary"></i>{" "}
+                    {item.email}
+                  </p>
+                  <p class="text-muted mb-0 mt-2">
+                    <i class="fas fa-lg fa-building font-size-15 align-middle pe-2 text-primary"></i>{" "}
+                    {item.address}
+                  </p>
+                </div>
+                <div class="d-flex gap-2 pt-4">
+                
+                  <Link type="button" class="btn btn-secondary btn-sm w-100"  to={`/customers/${item.id}`}>
+                    <i class="fas fa-sm fa-user me-1"></i> Info
+                  </Link>
+                </div>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+      <div class="row g-0 align-items-center pb-4">
+        <div class="col-sm-6">
+          <div>
+            
+          </div>
+        </div>
+        <div class="col-sm-6">
+          <div class="float-sm-end">
+            <ReactPaginate
+              previousLabel={"← Previous"}
+              nextLabel={"Next →"}
+              pageCount={pageCount}
+              onPageChange={handlePageClick}
+              activeClassName={"active"}
+              containerClassName={"pagination justify-content-center"}
+              pageClassName={"page-item"}
+              pageLinkClassName={"page-link"}
+              previousClassName={"page-item"}
+              previousLinkClassName={"page-link"}
+              nextClassName={"page-item"}
+              nextLinkClassName={"page-link"}
+              disabledClassName={"disabled"}
+            />
+          </div>
         </div>
       </div>
     </div>
--- src/pages/customers/showCustomer.js
+++ src/pages/customers/showCustomer.js
@@ -15,11 +15,8 @@ const ShowCustomer = () => {
   return (
     <div className="container-fluid px-4">
       <BreadCrumb currentPage={`Customer`} />
-      <div className="">
-        <div
-          className="container d-flex justify-content-center align-items-center"
-        
-        >
+      {/* <div className="">
+        <div className="container d-flex justify-content-center align-items-center">
           <div className="card profile-card shadow  mt-4">
             <div className="card-body text-center">
               <img
@@ -36,18 +33,97 @@ const ShowCustomer = () => {
 
               <ul className="list-group list-group-flush mb-3">
                 <li className="list-group-item bg-transparent">
-                  <i className="fas fa-lg fa-envelope"></i>{customer.email}
+                  <i className="fas fa-lg fa-envelope"></i>
+                  {customer.email}
                 </li>
                 <li className="list-group-item bg-transparent">
-                  <i className="fas fa-lg fa-building"></i>{customer.address}
+                  <i className="fas fa-lg fa-building"></i>
+                  {customer.address}
                 </li>
                 <li className="list-group-item bg-transparent">
-                  <i className="fas fa-lg fa-phone"></i>{customer.phone}
+                  <i className="fas fa-lg fa-phone"></i>
+                  {customer.phone}
                 </li>
               </ul>
-
-             
-
+            </div>
+          </div>
+        </div>
+      </div> */}
+      <div class="row">
+        <div class="col-lg-4">
+          <div class="card mb-4">
+            <div class="card-body text-center">
+              <img
+                src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-chat/ava3.webp"
+                alt="avatar"
+                class="rounded-circle img-fluid"
+                style={{width: 150+'px'}}
+              />
+              <h5 class="my-3">{customer.name}</h5>
+              <p class="text-muted mb-1">{customer.job}</p>
+              <p class="text-muted mb-4">{customer.address}</p>
+              <div class="d-flex justify-content-center mb-2">
+                <button
+                  type="button"
+                  data-mdb-button-init
+                  data-mdb-ripple-init
+                  class="btn btn-primary"
+                >
+                  Follow
+                </button>
+                <button
+                  type="button"
+                  data-mdb-button-init
+                  data-mdb-ripple-init
+                  class="btn btn-outline-primary ms-1"
+                >
+                  Message
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-8">
+          <div class="card mb-4">
+            <div class="card-body">
+              <div class="row">
+                <div class="col-sm-3">
+                  <p class="mb-0">Full Name</p>
+                </div>
+                <div class="col-sm-9">
+                  <p class="text-muted mb-0">{customer.name}</p>
+                </div>
+              </div>
+              <hr />
+              <hr />
+              <div class="row">
+                <div class="col-sm-3">
+                  <p class="mb-0">Email</p>
+                </div>
+                <div class="col-sm-9">
+                  <p class="text-muted mb-0">{customer.email}</p>
+                </div>
+              </div>
+              <hr />
+              <hr />
+              <div class="row">
+                <div class="col-sm-3">
+                  <p class="mb-0">Phone</p>
+                </div>
+                <div class="col-sm-9">
+                  <p class="text-muted mb-0">{customer.phone}</p>
+                </div>
+              </div>
+              <hr />
+              <hr />
+              <div class="row">
+                <div class="col-sm-3">
+                  <p class="mb-0">Address</p>
+                </div>
+                <div class="col-sm-9">
+                  <p class="text-muted mb-0">{customer.address}</p>
+                </div>
+              </div>
             </div>
           </div>
         </div>
